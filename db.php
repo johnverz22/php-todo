e889db0e8814cbb90d8713bfcbf070ead1d266ca
johnverz22 <johnny.verzola@lorma.edu>
2024-12-01 16:34:04 +0800

    Set SSL

--- db.php
+++ db.php
@@ -8,24 +8,36 @@ $password = getenv('DB_PASS') ? getenv('DB_PASS') : 'secret';
 
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password, [
+        PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // path to SSL  certificate file
+    ]);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
 
 
-// if using mysqli
+// If using SSL
 /*
+$path_to_ssl_ca = 'DigiCertGlobalRootCA.crt.pem';
+
 // Create connection using MySQLi
 $conn = new mysqli($host, $user, $password, $dbname, $port);
 
-// Check the connection
+// Set SSL options
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected successfully.";
+    // Enable SSL connection
+    if (!$conn->ssl_set(null, null, $path_to_ssl_ca)) {
+        die("Failed to set SSL parameters: " . $conn->error);
+    }
+
+    // Establish the connection after setting SSL
+    if ($conn->real_connect($host, $user, $password, $dbname, $port)) {
+        echo "Connected successfully with SSL.";
+    } else {
+        echo "Connection failed: " . $conn->connect_error;
+    }
 }
 */
-
-
